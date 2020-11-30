45ae8b3635e8336badf7db896479bbbeabefd230
Kunc <46520137+trikunc@users.noreply.github.com>
2020-11-30 09:58:52 +0700

    Calculator function finish

--- src/App.js
+++ src/App.js
@@ -1,61 +1,220 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-const BACKGROUND_COLOR = "#06121B";
-const BUTTON_PRIMARY_COLOR = "#0C3540";
-const BUTTON_SECONDARY_COLOR = "#3F4F5B";
-const BUTTON_EQUALS_COLOR = "#F27030";
+// const BACKGROUND_COLOR = "#06121B";
+// const BUTTON_PRIMARY_COLOR = "#0C3540";
+// const BUTTON_SECONDARY_COLOR = "#3F4F5B";
+// const BUTTON_EQUALS_COLOR = "#F27030";
 const GREY_FONT = "#848A8F";
 
 const App = () => {
-  const SubmitButton = () => {
-    return (
-      <div className="calculatorCell">
-        <div className="primaryButton">
-          <p className="primaryButtonText">=</p>
-        </div>
-      </div>
-    );
+  const [history, setHistory] = useState([]);
+  const [primaryDisplay, setPrimaryDisplay] = useState("0");
+  const [secondaryDisplay, setSecondaryDisplay] = useState("");
+  const [memory, setMemory] = useState("0");
+  const [mode, setMode] = useState(false);
+  const [num, setNum] = useState(true);
+  const [modeDisplay, setModeDisplay] = useState(false);
+  const [clear, setClear] = useState(true);
+  const [dot, setDot] = useState(false);
+  const [historyModal, setHistoryModal] = useState(false);
+
+  const onClickHandler = (value) => {
+    let newValue;
+    if (value === 0 && primaryDisplay === "0") {
+      return;
+    } else if (value === "." && primaryDisplay === "0") {
+      setPrimaryDisplay(`0.`);
+      setDot(true);
+    } else if (value !== 0 && primaryDisplay === "0") {
+      setPrimaryDisplay(value);
+      setModeDisplay(false);
+    } else if (
+      value === "×" ||
+      value === "÷" ||
+      value === "+" ||
+      value === "-" ||
+      value === "="
+    ) {
+      const result = secondaryDisplay[secondaryDisplay.length - 1];
+      if (
+        !num &&
+        (result === "×" || result === "÷" || result === "+" || result === "-")
+      ) {
+        if (value !== "-") {
+          if (value === "+") {
+            setPrimaryDisplay("+");
+          }
+          let newSecondaryDisplay = secondaryDisplay.split("");
+          newSecondaryDisplay.pop();
+          newSecondaryDisplay = newSecondaryDisplay.join("");
+          setSecondaryDisplay(`${newSecondaryDisplay}${value}`);
+        } else if (value === "-") {
+          setPrimaryDisplay("-");
+          setModeDisplay(false);
+        }
+      } else {
+        arithmatic(value);
+      }
+      setNum(false);
+    } else if (value === ".") {
+      console.log("dot");
+      dot
+        ? setPrimaryDisplay(primaryDisplay)
+        : setPrimaryDisplay(`${primaryDisplay}${value}`);
+      setDot(true);
+      console.log("dot:", dot);
+    } else if (
+      value !== "×" ||
+      value !== "÷" ||
+      value !== "+" ||
+      value !== "-" ||
+      value !== "="
+    ) {
+      newValue = modeDisplay ? `${value}` : `${primaryDisplay}${value}`;
+      setPrimaryDisplay(newValue);
+      setModeDisplay(false);
+      setNum(true);
+    }
   };
 
-  const PrimaryButton = (value) => {
+  const arithmatic = (value) => {
+    setDot(false);
+    mode
+      ? setSecondaryDisplay(`${secondaryDisplay}${primaryDisplay}${value}`)
+      : setSecondaryDisplay(`${primaryDisplay}${value}`);
+    setMemory(primaryDisplay);
+    if (value !== "=") {
+      mode ? getResult(value) : setMode(true);
+    } else if (value === "=") {
+      getResult(value);
+      setMode(false);
+    }
+    setModeDisplay(true);
+  };
+
+  const clearScreen = () => {
+    setSecondaryDisplay("");
+    setPrimaryDisplay("0");
+    setMemory("0");
+    setMode(false);
+    setDot(false);
+  };
+
+  const changeMod = () => {
+    const negMod = "-";
+    let result = "";
+    if (primaryDisplay.includes(negMod)) {
+      result = primaryDisplay.replace("-", "");
+    } else {
+      result = primaryDisplay === "0" ? primaryDisplay : `-${primaryDisplay}`;
+    }
+    setPrimaryDisplay(result);
+  };
+
+  const backClick = () => {
+    if (primaryDisplay.length > 1) {
+      let newPrimaryDisplay = primaryDisplay.split("");
+      newPrimaryDisplay.pop();
+      newPrimaryDisplay = newPrimaryDisplay.join("");
+      setPrimaryDisplay(newPrimaryDisplay);
+    } else if (primaryDisplay.length === 1) {
+      setPrimaryDisplay("0");
+    }
+  };
+
+  const getResult = (value) => {
+    setDot(false);
+    if (
+      mode &&
+      (value === "×" ||
+        value === "÷" ||
+        value === "+" ||
+        value === "-" ||
+        value === "=")
+    ) {
+      let popIndex = secondaryDisplay.length;
+      let popMod = secondaryDisplay[popIndex - 1];
+      let mem = 0;
+      let dis = 0;
+
+      if (popMod === "×") {
+        mem = parseFloat(memory) * parseFloat(primaryDisplay);
+        dis = memory * parseFloat(primaryDisplay);
+      }
+      if (popMod === "÷") {
+        mem = parseFloat(memory) / parseFloat(primaryDisplay);
+        dis = parseFloat(memory) / parseFloat(primaryDisplay);
+      } else if (popMod === "+") {
+        mem = parseFloat(memory) + parseFloat(primaryDisplay);
+        dis = parseFloat(memory) + parseFloat(primaryDisplay);
+      } else if (popMod === "-") {
+        mem = parseFloat(memory) - parseFloat(primaryDisplay);
+        dis = parseFloat(memory) - parseFloat(primaryDisplay);
+      }
+
+      setMemory(mem);
+      setPrimaryDisplay(dis);
+    }
+  };
+
+  const toogleModal = () => {
+    setHistoryModal(!historyModal);
+  };
+
+  const PrimaryButton = (value, id, functionCall = null) => {
     return (
       <div className="calculatorCell">
-        <button className="primaryButton">
+        <button
+          className="primaryButton"
+          id={id}
+          onClick={() =>
+            functionCall ? functionCall(value) : onClickHandler(value)
+          }
+        >
           <p className="primaryButtonText">{value}</p>
         </button>
       </div>
     );
   };
 
-  const SecondaryButton = (value) => {
+  const SecondaryButton = (value, id) => {
     return (
       <div className="calculatorCell">
-        <button className="secondaryButton">
+        <button
+          className="secondaryButton"
+          id={id}
+          onClick={() => onClickHandler(value)}
+        >
           <p className="secondaryButtonText">{value}</p>
         </button>
       </div>
     );
   };
 
-  const SecondaryButtonDouble = (value) => {
+  const SecondaryButtonDouble = (value, id) => {
     return (
       <div className="calculatorCellDouble">
-        <div className="secondaryButton" style={{ width: "99%" }}>
+        <button
+          className="secondaryButton"
+          id={id}
+          style={{ width: "99%" }}
+          onClick={() => onClickHandler(value)}
+        >
           <p className="secondaryButtonText">{value}</p>
-        </div>
+        </button>
       </div>
     );
   };
 
   return (
     <div className="container">
-      <div id="display" className="display">
+      <div className="display">
         {/* <div className="displayContainer"> */}
         <div
           style={{ width: "100%", flexDirection: "row-reverse", marginTop: 30 }}
         >
-          <p style={{ fontSize: 20, color: GREY_FONT }}>Test secondary</p>
+          <p style={{ fontSize: 20, color: GREY_FONT }}>{secondaryDisplay}</p>
         </div>
         <div
           style={{
@@ -65,51 +224,56 @@ const App = () => {
             marginBottom: 30
           }}
         >
-          <p style={{ fontSize: 28, color: "white" }}>Test primary</p>
+          <p id="display" style={{ fontSize: 28, color: "white" }}>
+            {primaryDisplay}
+          </p>
         </div>
 
         {/* </div> */}
       </div>
       <div className="bodyCal">
         <div className="symbol">
-          <button>
-            <p className="symbolButton Hist">HISTORY</p>
+          <button onClick={() => toogleModal()}>
+            <p className="symbolButton Hist">
+              {historyModal ? "KEYPAD" : "HISTORY"}
+            </p>
           </button>
-          <button>
+          <button onClick={() => backClick()}>
             <p className="symbolButton Back">
               <i className="fas fa-backspace fa-2x" />
             </p>
           </button>
         </div>
         <div className="topRow">
-          {PrimaryButton("C")}
-          {PrimaryButton("+/-")}
-          {PrimaryButton("%")}
-          {PrimaryButton("÷")}
+          {PrimaryButton("C", "clear", clearScreen)}
+          {PrimaryButton("+/-", "chengeMod", changeMod)}
+          {PrimaryButton("%", "percentage")}
+          {PrimaryButton("÷", "divide")}
         </div>
         <div className="botRow">
-          {SecondaryButton(7)}
-          {SecondaryButton(8)}
-          {SecondaryButton(9)}
-          {PrimaryButton("x")}
+          {SecondaryButton(7, "seven")}
+          {SecondaryButton(8, "eight")}
+          {SecondaryButton(9, "nine")}
+          {PrimaryButton("×", "multiply")}
         </div>
 
         <div className="botRow">
-          {SecondaryButton(4)}
-          {SecondaryButton(5)}
-          {SecondaryButton(6)}
-          {PrimaryButton("-")}
+          {SecondaryButton(4, "four")}
+          {SecondaryButton(5, "five")}
+          {SecondaryButton(6, "six")}
+          {PrimaryButton("-", "subtract")}
         </div>
         <div className="botRow">
-          {SecondaryButton(1)}
-          {SecondaryButton(2)}
-          {SecondaryButton(3)}
-          {PrimaryButton("+")}
+          {SecondaryButton(1, "one")}
+          {SecondaryButton(2, "two")}
+          {SecondaryButton(3, "three")}
+          {PrimaryButton("+", "add")}
         </div>
         <div className="botRow">
-          {SecondaryButtonDouble(0)}
-          {SecondaryButton(".")}
-          {SubmitButton()}
+          {SecondaryButtonDouble(0, "zero")}
+          {SecondaryButton(".", "decimal")}
+          {PrimaryButton("=", "equals")}
+          {/* {SubmitButton()} */}
         </div>
       </div>
     </div>
